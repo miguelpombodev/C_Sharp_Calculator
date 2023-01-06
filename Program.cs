4b0d714cb248df09b43cf0c66e25fdf46c7e5389
Miguel Pombo <mp96dev@gmail.com>
2023-01-05 23:26:55 -0300

    feat: implementing multiplication operation

--- Program.cs
+++ Program.cs
@@ -65,6 +65,26 @@ namespace Name
 
             result = value1/value2;
             Console.WriteLine($"O resultado da soma é: {result}");
-        }        
+        }
+        
+        static void Multiplication() {
+            float value1;
+            float value2;
+            float result;
+
+            Console.Clear();
+            Console.WriteLine("Primeiro valor: ");
+
+            value1 = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("Segundo valor: ");
+            value2 = float.Parse(Console.ReadLine()); 
+
+
+            Console.WriteLine("");
+
+            result = value1*value2;
+            Console.WriteLine($"O resultado da soma é: {result}");            
+        }
     }    
 }
