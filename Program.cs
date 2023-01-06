4ce63f88c4544e66a07a433b6feeeaa3a3054324
Miguel Pombo <mp96dev@gmail.com>
2023-01-05 23:25:55 -0300

    feat: implementing subtraction operation

--- Program.cs
+++ Program.cs
@@ -4,6 +4,10 @@ namespace Name
 {
     class Program {
         static void Main(string[] args) {
+            Add();
+        }
+
+        static void Add(){
             float value1;
             float value2;
             float result;
@@ -20,6 +24,26 @@ namespace Name
             Console.WriteLine("");
 
             result = value1 + value2;
+            Console.WriteLine($"O resultado da soma é: {result}");
+        }
+
+        static void Subtraction() {
+            float value1;
+            float value2;
+            float result;
+
+            Console.Clear();
+            Console.WriteLine("Primeiro valor: ");
+
+            value1 = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("Segundo valor: ");
+            value2 = float.Parse(Console.ReadLine()); 
+
+
+            Console.WriteLine("");
+
+            result = value1 - value2;
             Console.WriteLine($"O resultado da soma é: {result}");
         }
     }    
