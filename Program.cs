f6c837c3f32267a57da99821916e60cad0e40f56
Miguel Pombo <mp96dev@gmail.com>
2023-01-05 23:00:36 -0300

    feat: init project

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Name
+{
+    class Program {
+        static void Main(string[] args) {
+            Console.Clear();
+            Console.WriteLine("Primeiro valor: ");
+
+            float valor1 = float.Parse(Console.ReadLine());
+
+            Console.WriteLine(valor1);
+        }
+    }    
+}
