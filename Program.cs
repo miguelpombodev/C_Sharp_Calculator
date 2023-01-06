f0293d1180f351327e3af8073e9fb318238f16b6
Miguel Pombo <mp96dev@gmail.com>
2023-01-05 23:26:17 -0300

    feat: implementing division operation

--- Program.cs
+++ Program.cs
@@ -46,5 +46,25 @@ namespace Name
             result = value1 - value2;
             Console.WriteLine($"O resultado da soma é: {result}");
         }
+
+        static void Division() {
+            float value1;
+            float value2;
+            float result;
+
+            Console.Clear();
+            Console.WriteLine("Primeiro valor: ");
+
+            value1 = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("Segundo valor: ");
+            value2 = float.Parse(Console.ReadLine()); 
+
+
+            Console.WriteLine("");
+
+            result = value1/value2;
+            Console.WriteLine($"O resultado da soma é: {result}");
+        }        
     }    
 }
